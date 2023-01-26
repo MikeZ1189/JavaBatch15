ed779013ffefa485d11fc3faf65882ace4a15771
MikeZ1189 <zmich89@vt.edu>
2023-01-25 22:06:55 -0500

    class14

---
+++ src/class14/ReturnDemo.java
@@ -0,0 +1,11 @@
+package class14;
+
+public class ReturnDemo {
+    String method1(){
+        return "Hello World";
+    }
+    int method2(){
+        //return = the output of the method
+        return 20;
+    }
+}
