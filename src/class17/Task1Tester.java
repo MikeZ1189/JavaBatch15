d8be0ed9c4d7677084334c91eddc18c56551f4c3
MikeZ1189 <zmich89@vt.edu>
2023-02-04 11:40:34 -0500

    class17

---
+++ src/class17/Task1Tester.java
@@ -0,0 +1,13 @@
+package class17;
+
+import class18.Task1;
+
+public class Task1Tester {
+    //Task1 task1=new Task1();
+    //class18.Task1 task2=new class18.Task1("Java");
+    //class18.Task1 task3=new class18.Task1(1000);
+   // class18.Task1 task4=new Task1('G');
+    // outside of a package only public or protected is allowed.
+    // for protected keyword to work we need to have
+    //a paranet child relationship
+}
