1a0065497fea7c10ec6a079385628ae96ecb58a8
MikeZ1189 <zmich89@vt.edu>
2023-03-10 22:35:59 -0500

    reviewclass11

--- src/class14/Math.java
+++ src/class14/Math.java
@@ -7,6 +7,7 @@ public class Math {
     }
 
     void mul(int num1, int num2, int num3) {
+
         System.out.println(num1 * num2 * num3);
     }
         int sub(int num1, int num2){
--- src/class14/MathTester.java
+++ src/class14/MathTester.java
@@ -11,6 +11,6 @@ public class MathTester {
        // Scanner scanner=new Scanner (System.in);
         // int num= scanner.nextInt();
         int result=math.sub(100,50);
-        System.out.println(result);
+       System.out.println(result);
     }
 }
